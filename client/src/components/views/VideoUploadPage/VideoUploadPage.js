6f0b88f736a2893e47b3250d3806a095b0cd6980
4802852 <4802852@gmail.com>
2022-09-09 01:12:40 +0900

    video upload page

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Typography, Button, Form, message, Input } from "antd";
 import Dropzone from "react-dropzone";
 import { AiOutlinePlus } from "react-icons/ai";
@@ -6,7 +6,40 @@ import { AiOutlinePlus } from "react-icons/ai";
 const { Title } = Typography;
 const { TextArea } = Input;
 
+const PrivateOption = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "Public" },
+];
+
+const CategoryOption = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
+
 function VideoUploadPage() {
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("Film & Animation");
+
+  const onTitleChange = (e) => {
+    setVideoTitle(e.currentTarget.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value);
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -15,7 +48,7 @@ function VideoUploadPage() {
       <Form onDrop multiple maxSize>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop Zone */}
-          <Dropzone>
+          <Dropzone onDrop multiple maxSize>
             {({ getRootProps, getInputProps }) => (
               <div style={{ display: "flex", width: "300px", height: "240px", border: "1px solid lightgray", alignItems: "center", justifyContent: "center" }} {...getRootProps()}>
                 <input {...getInputProps()} />
@@ -31,20 +64,28 @@ function VideoUploadPage() {
         <br />
         <br />
         <label>Title</label>
-        <Input onChange value />
+        <Input onChange={onTitleChange} value={VideoTitle} />
         <br />
         <br />
         <label>Description</label>
-        <TextArea onChange value />
+        <TextArea onChange={onDescriptionChange} value={Description} />
         <br />
         <br />
-        <select onChange>
-          <option key value></option>
+        <select onChange={onPrivateChange}>
+          {PrivateOption.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
-        <select onChange>
-          <option key value></option>
+        <select onChange={onCategoryChange}>
+          {CategoryOption.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
         </select>
         <br />
         <br />
